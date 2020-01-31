d06160ef8fd548dded6c2d162165aef289ab0a51
Brian Hannafious <bhannafi@ucsc.edu>
2020-01-31 14:28:08 -0800

    Refactor copy code, include oneshots

--- terra_notebook_utils/drs.py
+++ terra_notebook_utils/drs.py
@@ -53,38 +53,6 @@ def copy(drs_url: str, dst_key: str, dst_bucket_name: str=None):
     src_bucket_name, src_key = _parse_gs_url(data_url)
     src_client = gs.get_client(credentials_data, project=credentials_data['project_id'])
     dst_client = gs.get_client()
+    src_bucket = src_client.bucket(src_bucket_name, user_project=WORKSPACE_GOOGLE_PROJECT)
     dst_bucket = dst_client.bucket(dst_bucket_name)
-
-    dst_blob_names: list = list()
-    for chunk in gs.iter_download(src_client, src_bucket_name, src_key, WORKSPACE_GOOGLE_PROJECT):
-        part_number = len(dst_blob_names) + 1
-        blob_name = f"{dst_key}.part{part_number}"
-        dst_blob_names.append(blob_name)
-        print("Uploading part", blob_name)
-        dst_bucket.blob(blob_name).upload_from_file(io.BytesIO(chunk))
-    gs.compose_parts(dst_bucket, dst_blob_names, dst_key)
-
-def old_copy(drs_url: str, dst_key: str, dst_bucket: str=None):
-    """
-    Resolve `drs_url` and copy into user-specified bucket `dst_bucket`.
-    If `dst_bucket` is None, copy into workspace bucket.
-    """
-    if dst_bucket is None:
-        dst_bucket = WORKSPACE_BUCKET
-    data_url, credentials_data = _resolve_drs_for_gs_storage(drs_url)
-    src_bucket, src_key = _parse_gs_url(data_url)
-    src_client = gs.get_client(credentials_data, project=credentials_data['project_id'])
-    dst_client = gs.get_client()
-    src_blob = src_client.bucket(src_bucket, user_project=WORKSPACE_GOOGLE_PROJECT).blob(src_key)
-    dst_blob = dst_client.bucket(dst_bucket, user_project=WORKSPACE_GOOGLE_PROJECT).blob(dst_key)
-    with NamedTemporaryFile("wb") as fh_write:
-        src_blob.download_to_file(fh_write)
-        fh_write.flush()
-        with open(fh_write.name, "rb") as fh_read:
-            dst_blob.upload_from_file(fh_read)
-
-# genomes/NWD522743.b38.irc.v1.cram.crai
-# download url
-# https://storage.gsapis.com/download/storage/v1/b/topmed-irc-share/o/genomes%2FNWD522743.b38.irc.v1.cram.crai?alt=media&userProject=firecloud-cgl
-# upload url
-# https://storage.gsapis.com/upload/storage/v1/b/fc-d500be74-3672-458e-8e89-662a08922941/o?uploadType=resumable&userProject=firecloud-cgl
+    gs.copy(src_bucket, dst_bucket, src_key, dst_key)
--- terra_notebook_utils/gs.py
+++ terra_notebook_utils/gs.py
@@ -1,21 +1,22 @@
 import io
 import os
 import json
+import threading
 from urllib.parse import quote
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from google.cloud.storage import Client
 from google.oauth2 import service_account
 from google.resumable_media.requests import ChunkedDownload
+import google.auth
 
-chunk_size = 1024 * 1024 * 4
+chunk_size = 1024 * 1024 * 32
 
 def get_access_token():
     """
-    Retriev the access token using the default GCP account
+    Retrieve the access token using the default GCP account
     returns the same result as `gcloud auth print-access-token`
     """
-
-    import google.auth.transport.requests
     if os.environ.get("TERRA_NOTEBOOK_GOOGLE_ACCESS_TOKEN"):
         token = os.environ['TERRA_NOTEBOOK_GOOGLE_ACCESS_TOKEN']
     elif os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"):
@@ -25,6 +26,7 @@ def get_access_token():
                                                                  scopes=scopes)
         token = creds.get_access_token().access_token
     else:
+        import google.auth.transport.requests
         creds, projects = google.auth.default()
         creds.refresh(google.auth.transport.requests.Request())
         token = creds.token
@@ -37,21 +39,59 @@ def get_client(credentials_data: dict=None, project: str=None):
         kwargs['credentials'] = creds
     if project is not None:
         kwargs['project'] = project
-    return Client(**kwargs)
+    client = Client(**kwargs)
+    if credentials_data is None:
+        client._credentials.refresh(google.auth.transport.requests.Request())
+    return client
 
-def iter_download(client, bucket_name: str, key: str, user_project: str=None):
+def chunked_download(bucket, key: str):
     blob_name = quote(key, safe="")
-    download_url = f"https://storage.googleapis.com/download/storage/v1/b/{bucket_name}/o/{blob_name}?alt=media"
-    if user_project is not None:
-        download_url += f"&userProject={user_project}"
+    download_url = f"https://storage.googleapis.com/download/storage/v1/b/{bucket.name}/o/{blob_name}?alt=media"
+    if bucket.user_project is not None:
+        download_url += f"&userProject={bucket.user_project}"
     stream = io.BytesIO()
     download = ChunkedDownload(download_url, chunk_size, stream)
     while not download.finished:
-        resp = download.consume_next_chunk(client._http)
+        resp = download.consume_next_chunk(bucket.client._http)
         stream.seek(0)
         yield resp.content
 
-def compose_parts(bucket, blob_names, dst_key):
+def oneshot_copy(src_bucket, dst_bucket, src_key, dst_key):
+    """
+    Download an object into memory from `src_bucket` and upload it to `dst_bucket`
+    """
+    fh = io.BytesIO()
+    src_bucket.blob(src_key).download_to_file(fh)
+    fh.seek(0)
+    dst_bucket.blob(dst_key).upload_from_file(fh)
+
+def multipart_copy(src_bucket, dst_bucket, src_key, dst_key):
+    """
+    Download an upjoct in chunks from `src_bucket` and upload each chunk to `dst_bucket` as separate objets.
+    The objects are then composed into a single object, and the parts are deleted from `dst_bucket`.
+    """
+    lock = threading.Lock()
+
+    def upload_chunk(chunk, blob_name):
+        with lock:
+            print("Uploading part", blob_name)
+        dst_bucket.blob(blob_name).upload_from_file(io.BytesIO(chunk))
+        del chunk
+
+    dst_blob_names: list = list()
+    with ThreadPoolExecutor(max_workers=4) as e:
+        futures = list()
+        for chunk in chunked_download(src_bucket, src_key):
+            part_number = len(dst_blob_names) + 1
+            blob_name = f"{dst_key}.part{part_number}"
+            dst_blob_names.append(blob_name)
+            futures.append(e.submit(upload_chunk, chunk, blob_name))
+        for f in as_completed(futures):
+            f.result()
+    _compose_parts(dst_bucket, dst_blob_names, dst_key)
+
+def _compose_parts(bucket, blob_names, dst_key):
+    # TODO: extend this to work with num_parts > 32
     blobs = [bucket.get_blob(b) for b in blob_names]
     dst_blob = bucket.blob(dst_key)
     dst_blob.compose(blobs)
@@ -61,8 +101,10 @@ def compose_parts(bucket, blob_names, dst_key):
         except Exception:
             pass
 
-def oneshot_copy():
-    pass
-
-def multipart_copy():
-    pass
+def copy(src_bucket, dst_bucket, src_key, dst_key):
+    src_blob = src_bucket.blob(src_key)
+    src_blob.reload()
+    if chunk_size >= src_blob.size:
+        oneshot_copy(src_bucket, dst_bucket, src_key, dst_key)
+    else:
+        multipart_copy(src_bucket, dst_bucket, src_key, dst_key)
