6a7e3b5de575831cd6c6619936fa1f66c2b03ab4
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-02-07 09:27:12 -0800

    Unarchive tar.gz files to GS bucket

---
+++ terra_notebook_utils/tar_gz.py
@@ -0,0 +1,54 @@
+import io
+import os
+import gzip
+import typing
+import tarfile
+from math import ceil
+from pathlib import Path
+from contextlib import closing
+
+from terra_notebook_utils import gs
+from terra_notebook_utils.progress_bar import ProgressBar
+
+_chunk_size = 1024 * 1024 * 3
+
+def extract(src_fh, dst_bucket: typing.Optional[gs.Bucket]=None, root: typing.Optional[str]=None):
+    """
+    Extract a tar.gz archive into the local filesystem, or a GS bucket if `dst_bucket` is provided.
+    """
+    gzip_reader = gzip.GzipFile(fileobj=src_fh)
+    tf = tarfile.TarFile(fileobj=gzip_reader)  # type: ignore
+    for tarinfo in tf:
+        if tarinfo.isfile():
+            if dst_bucket:
+                dst_fh = _prepare_gs(tarinfo, dst_bucket, root)
+            else:
+                dst_fh = _prepare_local(tarinfo, root)
+            print(f"Inflating {tarinfo.name}")
+            progress_bar = ProgressBar(ceil(tarinfo.size / _chunk_size),
+                                       suffix=f"{ceil(tarinfo.size / _chunk_size)}MB")
+            with closing(progress_bar):
+                with closing(dst_fh):
+                    _transfer_data(tf.extractfile(tarinfo), dst_fh, progress_bar)
+
+def _transfer_data(from_fh, to_fh, progress_bar: ProgressBar=None):
+    while True:
+        data = from_fh.read(_chunk_size)
+        if data:
+            to_fh.write(data)
+        else:
+            break
+        if progress_bar:
+            progress_bar.update()
+
+def _prepare_local(tarinfo: tarfile.TarInfo, root: typing.Optional[str]):
+    filepath = os.path.abspath(os.path.join(root, tarinfo.name))
+    Path(os.path.dirname(filepath)).mkdir(parents=True, exist_ok=True)
+    return open(filepath, "wb")
+
+def _prepare_gs(tarinfo: tarfile.TarInfo, bucket: gs.Bucket, root: typing.Optional[str]):
+    if root is not None:
+        key = f"{root}/{tarinfo.name}"
+    else:
+        key = tarinfo.name
+    return gs.ChunkedWriter(key, bucket, chunk_size=_chunk_size)
