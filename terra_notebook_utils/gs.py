70cd1d806a0e5e2fe412dad4211750c110daf0cc
Brian Hannafious <brian@connectedsignals.com>
2020-03-02 09:08:25 -0800

    wip

--- terra_notebook_utils/gs.py
+++ terra_notebook_utils/gs.py
@@ -80,14 +80,13 @@ def multipart_copy(src_bucket, dst_bucket, src_key, dst_key):
     src_blob = src_bucket.get_blob(src_key)
     print(f"Copying from {src_bucket.name}/{src_key}")
     print(f"Copying to {dst_bucket.name}/{dst_key}")
-    e = ThreadPoolExecutor(max_workers=4)
-    with gscio.AsyncReader(src_blob, chunks_to_buffer=2) as reader:
+    with gscio.AsyncReader(src_blob) as reader:
         progress_bar = ProgressBar(1 + reader.number_of_chunks(),
                                    prefix="Copying:",
                                    size=src_blob.size // 1024 ** 2,
                                    units="MB")
         with closing(progress_bar):
-            with gscio.AsyncWriter(dst_key, dst_bucket, executor=e) as writer:
+            with gscio.AsyncWriter(dst_key, dst_bucket) as writer:
                 for chunk in reader.for_each_chunk():
                     writer.write(chunk)
                     progress_bar.update()
