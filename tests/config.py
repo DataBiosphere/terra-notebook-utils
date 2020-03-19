4275a4e36d07ae2a76be229673c27cfc4a647e2d
Brian Hannafious <brian@connectedsignals.com>
2020-03-18 19:32:23 -0700

    tests

--- tests/config.py
+++ tests/config.py
@@ -1,5 +1,5 @@
 import os
 
 os.environ['GOOGLE_PROJECT'] = "firecloud-cgl"
-os.environ['WORKSPACE_NAME'] = "xbrianh-gen3-vcf"
-os.environ['WORKSPACE_BUCKET'] = "gs://fc-d500be74-3672-458e-8e89-662a08922941"
+os.environ['WORKSPACE_NAME'] = "terra-notebook-utils-tests"
+os.environ['WORKSPACE_BUCKET'] = "gs://fc-9169fcd1-92ce-4d60-9d2d-d19fd326ff10"
---
+++ tests/fixtures/populate_workspace_data.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""
+Upload fixtures into Terra data model
+"""
+import os
+import sys
+import json
+
+from firecloud import fiss
+
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', ".."))  # noqa
+sys.path.insert(0, pkg_root)  # noqa
+
+import tests.config
+WORKSPACE_GOOGLE_PROJECT = os.environ['GOOGLE_PROJECT']
+WORKSPACE_NAME = os.environ['WORKSPACE_NAME']
+
+def list_entities():
+    resp = fiss.fapi.get_entities_with_type(WORKSPACE_GOOGLE_PROJECT, WORKSPACE_NAME)
+    resp.raise_for_status()
+    for ent in resp.json():
+        yield ent
+
+def delete_entities(entities):
+    resp = fiss.fapi.delete_entities(WORKSPACE_GOOGLE_PROJECT, WORKSPACE_NAME, entities)
+    resp.raise_for_status()
+
+def delete_all_entities():
+    entities_to_delete = [dict(entityType=e['entityType'], entityName=e['name'])
+                          for e in list_entities()]
+    delete_entities(entities_to_delete)
+
+def upload_entities(tsv_data):
+    resp = fiss.fapi.upload_entities(WORKSPACE_GOOGLE_PROJECT, WORKSPACE_NAME, tsv_data, model="flexible")
+    print(resp.content)
+    resp.raise_for_status()
+
+delete_all_entities()
+with open("tests/fixtures/workspace_manifest.json", "rb") as fh:
+    manifest = json.loads(fh.read())
+    for table_name, vals in manifest.items():
+        tsv = "\t".join([f"entity:{table_name}_id", "object_id", "md5sum", "file_name", "file_size"])
+        for e in vals:
+            tsv += os.linesep + "\t".join([f"{e['uuid']}",
+                                           f"drs://{e['object_id']}",
+                                           e['md5sum'],
+                                           e['file_name'],
+                                           str(e['file_size'])])
+        upload_entities(tsv)
