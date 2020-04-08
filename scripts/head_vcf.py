577d45407ca079ec94a949106ef5a60f0fac7868
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-04-08 13:07:46 -0600

    Add head_vcf.py script (#18)
    Co-authored-by: Brian Hannafious <brian@connectedsignals.com>

---
+++ scripts/head_vcf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import io
+import argparse
+from contextlib import closing
+
+import bgzip
+
+parser = argparse.ArgumentParser()
+parser.add_argument("cloudpath", help="location of file. Can be local or GCP bucket path (e.g. gs://key)")
+args = parser.parse_args()
+
+if args.cloudpath.startswith("gs://"):
+    from google.cloud.storage import Client
+    import gs_chunked_io as gscio
+    cloudpath = args.cloudpath.split("gs://", 1)[1] 
+    bucket_name, key = cloudpath.split("/", 1)
+    blob = Client().bucket(bucket_name).get_blob(key)
+    raw = gscio.Reader(blob)
+else:
+    raw = open(args.cloudpath, "rb")
+
+with closing(raw):
+    with bgzip.BGZipReader(raw) as bgreader:
+        with io.BufferedReader(bgreader) as reader:
+            for line in reader:
+                l = line.decode("utf-8").strip()
+                if not l:
+                    continue
+                elif l.startswith("#"):
+                    print(l)
+                else:
+                    break
