3ace7a0fd28b16b732d517ff9f2b77ecc18eae5f
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-04-10 11:38:01 -0600

    Try harder to find google project env var (#23)

--- terra_notebook_utils/__init__.py
+++ terra_notebook_utils/__init__.py
@@ -3,8 +3,9 @@ import os
 WORKSPACE_NAME = os.environ.get('WORKSPACE_NAME', None)
 WORKSPACE_GOOGLE_PROJECT = os.environ.get('GOOGLE_PROJECT')  # This env var is set in Terra notebooks
 if not WORKSPACE_GOOGLE_PROJECT:
-    WORKSPACE_GOOGLE_PROJECT = os.environ.get('GCLOUD_PROJECT')  # Useful for running outside of notebook
-assert WORKSPACE_GOOGLE_PROJECT
+    WORKSPACE_GOOGLE_PROJECT = os.environ.get('GCP_PROJECT')  # Useful for running outside of notebook
+if not WORKSPACE_GOOGLE_PROJECT:
+    WORKSPACE_GOOGLE_PROJECT = os.environ.get('GCLOUD_PROJECT')  # Fallback
 # For a list of gcloud project related environment variables, see:
 # https://cloud.google.com/functions/docs/env-var#environment_variables_set_automatically
 WORKSPACE_BUCKET = os.environ.get('WORKSPACE_BUCKET', None)
