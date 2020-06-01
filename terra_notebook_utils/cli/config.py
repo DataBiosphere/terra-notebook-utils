41d2257a2367515762423a899636c77664f8f289
Brian Hannafious <brian@connectedsignals.com>
2020-06-01 11:22:45 -0700

    simplify cli configuration

--- terra_notebook_utils/cli/config.py
+++ terra_notebook_utils/cli/config.py
@@ -10,15 +10,24 @@ from terra_notebook_utils.cli import Config, dispatch
 config_cli = dispatch.group("config", help=__doc__)
 
 
-@config_cli.command("set", arguments={
-    "--key": dict(choices=Config.info.keys()),
-    "--value": dict()
+@config_cli.command("set-workspace", arguments={
+    "value": dict()
 })
-def config_set(args: argparse.Namespace):
+def set_config_workspace(args: argparse.Namespace):
     """
-    Set a configuration value. Use `tnu config print` to see current settings.
+    Set workspace for cli commands
     """
-    Config.info[args.key] = args.value
+    Config.info["workspace"] = args.value
+    Config.write()
+
+@config_cli.command("set-workspace-google-project", arguments={
+    "value": dict()
+})
+def set_config_billing_project(args: argparse.Namespace):
+    """
+    Set billing project for cli commands
+    """
+    Config.info["workspace_google_project"] = args.value
     Config.write()
 
 @config_cli.command("print")
