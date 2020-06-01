5c80f45e624923eacb49f2bcc02f66febff1970b
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-06-01 13:47:13 -0600

    CLI command to upload table rows (#58)

--- terra_notebook_utils/cli/table.py
+++ terra_notebook_utils/cli/table.py
@@ -1,6 +1,7 @@
 """
 Terra data table commands
 """
+import os
 import json
 import typing
 import argparse
@@ -76,6 +77,32 @@ def get_cell(args: argparse.Namespace):
     for e in table.list_entities(args.table, args.namespace, args.workspace):
         if args.id == e['name']:
             print(e['attributes'][args.column])
+
+@table_cli.command("put-row", arguments={
+    "--table": dict(type=str, required=True, help="table name"),
+    "--id": dict(type=str, required=True, help="Row id. This should be unique for the table"),
+    "data": dict(type=str, nargs="+", help="list of key-value pairs")
+})
+def put_row(args: argparse.Namespace):
+    """
+    Put a row.
+    Example:
+    tnu table put-row \\
+    --table abbrv_merge_input \\
+    --id 1 \\
+    bucket=fc-9169fcd1-92ce-4d60-9d2d-d19fd326ff10 \\
+    input_keys=test_vcfs/a.vcf.gz,test_vcfs/b.vcf.gz \\
+    output_key=foo.vcf.gz
+    """
+    _resolve_workspace_and_namespace(args)
+    headers = [f"{args.table}_id"]
+    values = [args.id]
+    for pair in args.data:
+        key, val = pair.split("=")
+        headers.append(key)
+        values.append(val)
+    tsv = "\t".join(headers) + os.linesep + "\t".join(values)
+    table.upload_entities(tsv, args.namespace, args.workspace)
 
 def _resolve_workspace_and_namespace(args: argparse.Namespace):
     args.workspace = args.workspace or Config.info['workspace']
