59f04272e03dfd439f44a6c17ee2c7185a9ad5e4
Brian Hannafious <brian@connectedsignals.com>
2020-06-02 22:00:05 -0700

    Small refactors

--- terra_notebook_utils/__init__.py
+++ terra_notebook_utils/__init__.py
@@ -9,7 +9,8 @@ if not WORKSPACE_GOOGLE_PROJECT:
 # For a list of gcloud project related environment variables, see:
 # https://cloud.google.com/functions/docs/env-var#environment_variables_set_automatically
 WORKSPACE_BUCKET = os.environ.get('WORKSPACE_BUCKET', None)
-DRS_SCHEMA = 'drs://'
-GS_SCHEMA = 'gs://'
-if WORKSPACE_BUCKET is not None and WORKSPACE_BUCKET.startswith(GS_SCHEMA):
-    WORKSPACE_BUCKET = WORKSPACE_BUCKET[len(GS_SCHEMA):]  # Chop off the bucket schema
+_GS_SCHEMA = 'gs://'
+if WORKSPACE_BUCKET is not None and WORKSPACE_BUCKET.startswith(_GS_SCHEMA):
+    WORKSPACE_BUCKET = WORKSPACE_BUCKET[len(_GS_SCHEMA):]  # Chop off the bucket schema
+
+from terra_notebook_utils import drs, profile, table, vcf, workspace
