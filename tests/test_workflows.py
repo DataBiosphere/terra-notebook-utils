3515e70a4d179a912369c7d423f0b210bc0ba068
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-10-06 15:04:38 -0600

    Introduce workflows CLI (#175)
    * Introduce workflows CLI
    
    * add cli docstrings

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -3,15 +3,19 @@ import os
 import sys
 import json
 import unittest
+from unittest import mock
 
 import requests
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-from tests import config
+from tests import config  # initialize the test environment
+from tests import CLITestMixin
 from tests.infra.testmode import testmode
 from terra_notebook_utils import workflows
+from terra_notebook_utils import WORKSPACE_NAME, WORKSPACE_GOOGLE_PROJECT
+import terra_notebook_utils.cli.workflows
 
 
 @testmode("workspace_access")
@@ -28,6 +32,26 @@ class TestTerraNotebookUtilsWorkflows(unittest.TestCase):
     def test_get_workflow(self):
         workflow_id = workflows.get_submission(self.submission_id)['workflows'][0]['workflowId']
         workflows.get_workflow(self.submission_id, workflow_id)
+
+@testmode("workspace_access")
+class TestTerraNotebookUtilsWorkflowsCLI(CLITestMixin, unittest.TestCase):
+    common_kwargs = dict(workspace=WORKSPACE_NAME, google_billing_project=WORKSPACE_GOOGLE_PROJECT)
+
+    def test_list_submissions(self):
+        with mock.patch("terra_notebook_utils.workflows.list_submissions"):
+            self._test_cmd(terra_notebook_utils.cli.workflows.list_submissions)
+
+    def test_get_submission(self):
+        with mock.patch("terra_notebook_utils.workflows.get_submission", return_value=dict()):
+            self._test_cmd(terra_notebook_utils.cli.workflows.get_submission,
+                           submission_id="frank")
+
+    def test_get_workflow(self):
+        with mock.patch("terra_notebook_utils.workflows.get_submission", return_value=dict()):
+            with mock.patch("terra_notebook_utils.workflows.get_workflow", return_value=dict()):
+                self._test_cmd(terra_notebook_utils.cli.workflows.get_submission,
+                               submission_id="frank",
+                               workflow_id="bob")
 
 if __name__ == '__main__':
     unittest.main()
