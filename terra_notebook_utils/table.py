dc54a2a1535856ef90b42d7e9508abca7e5749a8
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2021-01-08 09:26:32 -0700

    Context manager to upload Terra data tables (#244)
    * Context manager to upload Terra data tables
    
    This transparantly handles typed data uploads to Terra. Uploads are
    performed in the background.

--- terra_notebook_utils/table.py
+++ terra_notebook_utils/table.py
@@ -1,10 +1,127 @@
 """
 Terra data table commands
 """
+import os
+from uuid import uuid4
+from collections import defaultdict, namedtuple
+from typing import Any, Dict, Iterable, List, Optional, Set, Tuple, Union
+
+import requests
 from firecloud import fiss
-from typing import Optional
 
 from terra_notebook_utils import WORKSPACE_GOOGLE_PROJECT, WORKSPACE_NAME
+from terra_notebook_utils.utils import _AsyncContextManager
+
+
+Row = namedtuple("Row", "name attributes")
+
+VALUE = Union[str, int, float, bool]
+ATTRIBUTES = Dict[str, Union[VALUE, Iterable[VALUE]]]
+UPDATE_OPS = List[Dict[str, Any]]
+COLUMN_HEADERS = Tuple[str, ...]
+ROW_LIKE = Tuple[str, ATTRIBUTES]
+
+class Writer(_AsyncContextManager):
+    """
+    Distribute row uploads across as few API calls as possible.
+    Uploads are performed in the background.
+    Also transparantly handles sequences, which the Firecloud API makes difficult. (These must be uploaded and modified
+    via separate API calls.)
+    """
+    def __init__(self, name: str, **kwargs):
+        self.name = name
+        self._init_request_data()
+        self._workspace_name = kwargs.get("workspace_name", WORKSPACE_NAME)
+        self._workspace_google_project = kwargs.get("workspace_google_project", WORKSPACE_GOOGLE_PROJECT)
+
+    def _init_request_data(self):
+        self._tsvs: Dict[COLUMN_HEADERS, str] = defaultdict(list)
+        self._row_update_request_data: Dict[COLUMN_HEADERS, List[Tuple[Row, UPDATE_OPS]]] = dict()
+
+    def _get_row_update_request_data(self, row: Row) -> List[Dict[str, Any]]:
+        request_data = list()
+        for name, val in row.attributes.items():
+            if isinstance(val, str):
+                update_ops = list()  # No Firecloud update operations needed for string values
+            elif isinstance(val, (int, float, bool)):
+                update_ops = [dict(op="AddUpdateAttribute", attributeName=name, addUpdateAttribute=val)]
+            elif hasattr(val, "__iter__"):
+                update_ops = [dict(op="RemoveAttribute", attributeName=name)]
+                types: Set[type] = set()
+                for m in val:
+                    update_ops.append(dict(op="AddListMember", attributeListName=name, newMember=m))
+                    types.add(type(m))
+                assert 1 == len(types)
+            request_data.extend(update_ops)
+        return request_data
+
+    def put_row(self, item: Union[ROW_LIKE, ATTRIBUTES]) -> str:
+        if isinstance(item, dict):
+            row = Row(f"{uuid4()}", item)
+        else:
+            row = Row(*item)
+        column_headers = tuple(sorted(row.attributes.keys()))
+        if column_headers not in self._tsvs:
+            self._tsvs[column_headers] = "\t".join([f"{self.name}_id", *column_headers])
+            self._row_update_request_data[column_headers] = list()
+        self._tsvs[column_headers] += (
+            os.linesep
+            + row.name + "\t"
+            + "\t".join(row.attributes[c] if isinstance(row.attributes[c], str)
+                        else "x"  # Dummy value to be replaced during row update API calls.
+                        for c in column_headers)
+        )
+        update_request_data = self._get_row_update_request_data(row)
+        if update_request_data:
+            self._row_update_request_data[column_headers].append((row, update_request_data))
+        self._upload(1024 * 20)
+        return row.name
+
+    def _upload(self, threshold: Optional[int]=None):
+        """
+        Schedule uploads for all TSVs of size equal to or greater than `threshold`.
+        If `threshold` is None, schedule uploads for all TSVs.
+        """
+        for column_headers, tsv in self._tsvs.copy().items():
+            if threshold is None or len(tsv) >= threshold:
+                row_updates = self._row_update_request_data.get(column_headers, list())
+                self.submit(self._do_fiss_upload, tsv, row_updates)
+                del self._tsvs[column_headers]
+                if row_updates:
+                    del self._row_update_request_data[column_headers]
+
+    def _do_fiss_upload(self, tsv: str, row_update_request_data: List[Tuple[Row, List[Dict[str, Any]]]]):
+        fiss.fapi.upload_entities(self._workspace_google_project,
+                                  self._workspace_name,
+                                  tsv,
+                                  model="flexible").raise_for_status()
+        for row, request_data in row_update_request_data:
+            if request_data:
+                self.submit(self._do_fiss_updates, row, request_data)
+
+    def _do_fiss_updates(self, row: Row, request_data: UPDATE_OPS, retry: int=0):
+        try:
+            fiss.fapi.update_entity(self._workspace_google_project,
+                                    self._workspace_name,
+                                    self.name,
+                                    row.name,
+                                    request_data).raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            if 500 == e.response.status_code:
+                # Firecloud occasionally throws 500 errors for succesful update operations.
+                # Check if we get the row we expect, retry otherwise.
+                if 5 > retry:
+                    cur_row = get_row(self.name, row.name)
+                    if row != cur_row:
+                        self.submit(self._do_fiss_updates, row, request_data, retry=retry + 1)
+                else:
+                    raise Exception(f"Ran out of retries updating row {row.name}") from e
+            else:
+                raise
+
+    def _prepare_for_exit(self):
+        if self._tsvs:
+            self._upload()
 
 def _iter_table(table: str,
                 workspace_name: Optional[str]=WORKSPACE_NAME,
