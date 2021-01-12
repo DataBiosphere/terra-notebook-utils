713d42b44efe04e40d93580bddab9dbbedf6c988
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2021-01-11 22:53:27 -0700

    Introduce table upload convenience methods (#256)

--- tests/test_table.py
+++ tests/test_table.py
@@ -25,18 +25,7 @@ class TestTerraNotebookUtilsTable(SuppressWarningsMixin, unittest.TestCase):
     @testmode("workspace_access")
     def test_table(self):
         table_name = f"test_{uuid4()}"
-        number_of_entities = 5
         table.delete(table_name)  # remove cruft from previous failed tests
-
-        id_column = [f"{uuid4()}" for _ in range(number_of_entities)]
-        foo_column = [f"{uuid4()}" for _ in range(number_of_entities)]
-        bar_column = [f"{uuid4()}" for _ in range(number_of_entities)]
-
-        with self.subTest("Upload table entities"):
-            tsv = "\t".join([f"entity:{table_name}_id", "foo", "bar"])
-            for i in range(number_of_entities):
-                tsv += "\r" + "\t".join((id_column[i], foo_column[i], bar_column[i]))
-            table.upload_entities(tsv)
 
         with self.subTest("delete table"):
             table.delete(table_name)
