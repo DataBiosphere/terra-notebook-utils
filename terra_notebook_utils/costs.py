e10f272419dafe382bbb3938aaf0d2e90c4ddc3c
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2021-01-13 15:01:00 -0700

    Simplify cost calculations (#261)

--- terra_notebook_utils/costs.py
+++ terra_notebook_utils/costs.py
@@ -1,27 +1,31 @@
 from math import ceil
 
 
-MINUTE = 60
+SECOND = 1.0
+MINUTE = SECOND * 60
+HOUR = MINUTE * 60
+DAY = HOUR * 24
+MONTH = DAY * 30.437  # Not critical to be exact
 
 class GCPCustomN1Cost:
     # Pricing information: https://cloud.google.com/compute/vm-instance-pricing#n1_custommachinetypepricing
-    on_demand_per_hour_per_cpu = 0.033174
-    on_demand_per_hour_per_gb = 0.004446
-    preemptible_per_hour_per_cpu = 0.00698
-    preemptible_per_hour_per_gb = 0.00094
+    on_demand_per_cpu = 0.033174 / HOUR
+    on_demand_per_gb = 0.004446 / HOUR
+    preemptible_per_cpu = 0.00698 / HOUR
+    preemptible_per_gb = 0.00094 / HOUR
 
     @classmethod
     def estimate(cls, cpus: int, memory_gb: float, runtime_seconds: float, preemptible: bool) -> float:
         # GCP Instance Billing Model:
         # https://cloud.google.com/compute/vm-instance-pricing#billingmodel
         if preemptible:
-            per_hour_per_cpu = cls.preemptible_per_hour_per_cpu
-            per_hour_per_gb = cls.preemptible_per_hour_per_gb
+            per_cpu = cls.preemptible_per_cpu
+            per_gb = cls.preemptible_per_gb
         else:
-            per_hour_per_cpu = cls.on_demand_per_hour_per_cpu
-            per_hour_per_gb = cls.on_demand_per_hour_per_gb
-        cost_duration_hours = ceil(MINUTE if MINUTE >= runtime_seconds else runtime_seconds) / 3600
-        cost_for_cpus = cost_duration_hours * cpus * per_hour_per_cpu
-        cost_for_mem = cost_duration_hours * memory_gb * per_hour_per_gb
+            per_cpu = cls.on_demand_per_cpu
+            per_gb = cls.on_demand_per_gb
+        cost_duration_hours = ceil(MINUTE if MINUTE >= runtime_seconds else runtime_seconds)
+        cost_for_cpus = cost_duration_hours * cpus * per_cpu
+        cost_for_mem = cost_duration_hours * memory_gb * per_gb
         cost = cost_for_cpus + cost_for_mem
         return cost
