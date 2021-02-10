a993e102713224bcf3e476d7b9c22fb0261bb1e0
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2021-02-10 17:08:19 -0500

    Support table cells with dict values (#274)

--- terra_notebook_utils/table.py
+++ terra_notebook_utils/table.py
@@ -4,7 +4,7 @@ Terra data table commands
 import os
 from uuid import uuid4
 from collections import defaultdict, namedtuple
-from typing import Any, Dict, Generator, Iterable, List, Optional, Set, Tuple, Union
+from typing import Any, Dict, Generator, Iterable, List, Mapping, Optional, Set, Tuple, Union
 
 import requests
 from firecloud import fiss
@@ -62,14 +62,19 @@ class Writer(_AsyncContextManager):
 
     def _get_row_update_request_data(self, row: Row) -> List[Dict[str, Any]]:
         request_data = list()
+        types: Set[type]
         for name, val in row.attributes.items():
             if isinstance(val, str):
                 update_ops = list()  # No Firecloud update operations needed for string values
             elif isinstance(val, (int, float, bool)):
                 update_ops = [dict(op="AddUpdateAttribute", attributeName=name, addUpdateAttribute=val)]
+            elif isinstance(val, Mapping):
+                types = {type(k) for k in val.keys()}
+                assert 1 == len(types) and {str} == types
+                update_ops = [dict(op="AddUpdateAttribute", attributeName=name, addUpdateAttribute=val)]
             elif hasattr(val, "__iter__"):
                 update_ops = [dict(op="RemoveAttribute", attributeName=name)]
-                types: Set[type] = set()
+                types = set()
                 for m in val:
                     update_ops.append(dict(op="AddListMember", attributeListName=name, newMember=m))
                     types.add(type(m))
