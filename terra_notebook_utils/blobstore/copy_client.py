c3400f19ce05c003f1122d82556f264828cf1ddc
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2021-06-09 11:55:25 -0600

    Refactor copy client to improve reporting (#310)

--- terra_notebook_utils/blobstore/copy_client.py
+++ terra_notebook_utils/blobstore/copy_client.py
@@ -19,9 +19,6 @@ AnyBlobStore = Union[GSBlobStore, URLBlobStore, LocalBlobStore]
 AnyBlob = Union[GSBlob, URLBlob, LocalBlob]
 CloudBlob = GSBlob
 
-def _finalize_copy(src_blob: AnyBlob, dst_blob: AnyBlob):
-    logger.info(f"Copied {src_blob.url} to {dst_blob.url}")
-
 def _download(src_blob: AnyBlob, dst_blob: LocalBlob):
     logger.debug(f"Starting download {src_blob.url} to {dst_blob.url}")
     dirname = os.path.dirname(dst_blob.url)
@@ -29,7 +26,6 @@ def _download(src_blob: AnyBlob, dst_blob: LocalBlob):
         os.makedirs(dirname, exist_ok=True)
     # The download methods for each Blob is expected to compute checksums
     src_blob.download(dst_blob.url)
-    _finalize_copy(src_blob, dst_blob)
 
 def _copy_intra_cloud(src_blob: AnyBlob, dst_blob: AnyBlob):
     # In general it is not necesary to compute checksums for intra cloud copies. The Storage services will do that for
@@ -41,9 +37,7 @@ def _copy_intra_cloud(src_blob: AnyBlob, dst_blob: AnyBlob):
     dst_blob.copy_from(src_blob)  # type: ignore
     if src_blob.cloud_native_checksum() != dst_blob.cloud_native_checksum():
         logger.error(f"Checksum failed for {src_blob.url} to {dst_blob.url}")
-        dst_blob.delete()
         raise BlobstoreChecksumError()
-    _finalize_copy(src_blob, dst_blob)
 
 def _copy_oneshot_passthrough(src_blob: AnyBlob, dst_blob: CloudBlob):
     logger.debug(f"Starting oneshot passthrough {src_blob.url} to {dst_blob.url}")
@@ -51,9 +45,7 @@ def _copy_oneshot_passthrough(src_blob: AnyBlob, dst_blob: CloudBlob):
     dst_blob.put(data)
     if not dst_blob.Hasher(data).matches(dst_blob.cloud_native_checksum()):
         logger.error(f"Checksum failed for {src_blob.url} to {dst_blob.url}")
-        dst_blob.delete()
         raise BlobstoreChecksumError()
-    _finalize_copy(src_blob, dst_blob)
 
 def _copy_multipart_passthrough(src_blob: AnyBlob, dst_blob: CloudBlob):
     logger.debug(f"Starting multipart passthrough {src_blob.url} to {dst_blob.url}")
@@ -64,9 +56,29 @@ def _copy_multipart_passthrough(src_blob: AnyBlob, dst_blob: CloudBlob):
             cs.update(part.data)
     if not cs.matches(dst_blob.cloud_native_checksum()):
         logger.error(f"Checksum failed for {src_blob.url} to {dst_blob.url}")
-        dst_blob.delete()
         raise BlobstoreChecksumError()
-    _finalize_copy(src_blob, dst_blob)
+
+def _do_copy(src_blob: AnyBlob, dst_blob: AnyBlob, multipart_threshold: int):
+    try:
+        if isinstance(dst_blob, LocalBlob):
+            _download(src_blob, dst_blob)
+        elif isinstance(src_blob, type(dst_blob)):
+            _copy_intra_cloud(src_blob, dst_blob)
+        elif isinstance(dst_blob, CloudBlob):
+            if src_blob.size() <= multipart_threshold:
+                _copy_oneshot_passthrough(src_blob, dst_blob)
+            else:
+                _copy_multipart_passthrough(src_blob, dst_blob)
+        else:
+            raise TypeError(f"Cannot handle copy operation for blob types '{type(src_blob)}' and '{type(dst_blob)}'")
+        logger.info(f"Copied {src_blob.url} to {dst_blob.url}")
+    except Exception:
+        logger.exception(f"copy failed: '{src_blob.url}' to '{dst_blob.url}'")
+        try:
+            dst_blob.delete()
+        finally:
+            pass
+        raise
 
 class CopyClient:
     multipart_threshold = default_chunk_size
@@ -76,17 +88,8 @@ class CopyClient:
         self._queue = ConcurrentHeap(self._executor, concurrency)
 
     def copy(self, src_blob: AnyBlob, dst_blob: AnyBlob):
-        size = src_blob.size()
-        priority = -size
-        if isinstance(dst_blob, LocalBlob):
-            self._queue.priority_put(priority, _download, src_blob, dst_blob)
-        elif isinstance(src_blob, type(dst_blob)):
-            self._queue.priority_put(priority, _copy_intra_cloud, src_blob, dst_blob)
-        else:
-            if size <= self.multipart_threshold:
-                self._queue.priority_put(priority, _copy_oneshot_passthrough, src_blob, dst_blob)
-            else:
-                self._queue.priority_put(priority, _copy_multipart_passthrough, src_blob, dst_blob)
+        priority = -src_blob.size()
+        self._queue.priority_put(priority, _do_copy, src_blob, dst_blob, self.multipart_threshold)
 
     def __enter__(self):
         return self
