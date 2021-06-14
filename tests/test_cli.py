785ca4159e3d252965f5aff6f8f7a687c4ae65f7
Brian Hannafious <bhannafi@ucsc.edu>
2021-06-14 10:39:33 -0700

    Reorganize cli code

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,15 +3,13 @@ import io
 import os
 import sys
 import json
-import typing
 import base64
 import unittest
 import argparse
 import subprocess
-import traceback
+from uuid import uuid4
 from unittest import mock
 from random import randint
-from uuid import uuid4
 from contextlib import redirect_stdout
 from tempfile import NamedTemporaryFile
 from typing import List
@@ -27,18 +25,18 @@ from tests.infra.testmode import testmode
 from terra_notebook_utils import gs, table, WORKSPACE_NAME, WORKSPACE_GOOGLE_PROJECT, WORKSPACE_BUCKET
 from terra_notebook_utils.cli import Config
 import terra_notebook_utils.cli
-import terra_notebook_utils.cli.config
-import terra_notebook_utils.cli.vcf
-import terra_notebook_utils.cli.workspace
-import terra_notebook_utils.cli.profile
-import terra_notebook_utils.cli.drs
-import terra_notebook_utils.cli.table
+import terra_notebook_utils.cli.commands.config
+import terra_notebook_utils.cli.commands.vcf
+import terra_notebook_utils.cli.commands.workspace
+import terra_notebook_utils.cli.commands.profile
+import terra_notebook_utils.cli.commands.drs
+import terra_notebook_utils.cli.commands.table
 from tests.infra import SuppressWarningsMixin, upload_data
 from tests.infra.partialize_vcf import partialize_vcf
 
 
 @testmode("workspace_access")
-class TestTerraNotebookUtilsCLI_Config(SuppressWarningsMixin, unittest.TestCase):
+class TestCLI_Config(SuppressWarningsMixin, unittest.TestCase):
     def test_config_print(self):
         workspace = f"{uuid4()}"
         workspace_namespace = f"{uuid4()}"
@@ -48,7 +46,7 @@ class TestTerraNotebookUtilsCLI_Config(SuppressWarningsMixin, unittest.TestCase)
                 args = argparse.Namespace()
                 out = io.StringIO()
                 with redirect_stdout(out):
-                    terra_notebook_utils.cli.config.config_print(args)
+                    terra_notebook_utils.cli.commands.config.config_print(args)
                 data = json.loads(out.getvalue())
                 self.assertEqual(data, dict(workspace=workspace, workspace_namespace=workspace_namespace))
 
@@ -88,15 +86,15 @@ class TestTerraNotebookUtilsCLI_Config(SuppressWarningsMixin, unittest.TestCase)
             with ConfigOverride(None, None, tf.name):
                 Config.write()
                 args = argparse.Namespace(workspace=new_workspace)
-                terra_notebook_utils.cli.config.set_config_workspace(args)
+                terra_notebook_utils.cli.commands.config.set_config_workspace(args)
                 args = argparse.Namespace(workspace_namespace=new_workspace_namespace)
-                terra_notebook_utils.cli.config.set_config_workspace_namespace(args)
+                terra_notebook_utils.cli.commands.config.set_config_workspace_namespace(args)
                 with open(tf.name) as fh:
                     data = json.loads(fh.read())
                 self.assertEqual(data, dict(workspace=new_workspace,
                                             workspace_namespace=new_workspace_namespace))
 
-class TestTerraNotebookUtilsCLI_VCF(SuppressWarningsMixin, CLITestMixin, unittest.TestCase):
+class TestCLI_VCF(SuppressWarningsMixin, CLITestMixin, unittest.TestCase):
     common_kwargs = dict(workspace=WORKSPACE_NAME, workspace_namespace=WORKSPACE_GOOGLE_PROJECT)
     vcf_drs_url = "drs://dg.4503/57f58130-2d66-4d46-9b2b-539f7e6c2080"
 
@@ -111,64 +109,65 @@ class TestTerraNotebookUtilsCLI_VCF(SuppressWarningsMixin, CLITestMixin, unittes
     @testmode("workspace_access")
     def test_head_vcf(self):
         with self.subTest("Test gs:// object"):
-            self._test_cmd(terra_notebook_utils.cli.vcf.head, path=self.gs_uri)
+            self._test_cmd(terra_notebook_utils.cli.commands.vcf.head, path=self.gs_uri)
 
         with self.subTest("Test local object"):
-            self._test_cmd(terra_notebook_utils.cli.vcf.head, path="tests/fixtures/non_block_gzipped.vcf.gz")
+            self._test_cmd(terra_notebook_utils.cli.commands.vcf.head, path="tests/fixtures/non_block_gzipped.vcf.gz")
 
     @testmode("controlled_access")
     def test_head_drs(self):
         with self.subTest("Test drs:// object"):
-            self._test_cmd(terra_notebook_utils.cli.vcf.head, path=self.vcf_drs_url)
+            self._test_cmd(terra_notebook_utils.cli.commands.vcf.head, path=self.vcf_drs_url)
 
     @testmode("workspace_access")
     def test_samples(self):
         with self.subTest("Test gs:// object"):
-            self._test_cmd(terra_notebook_utils.cli.vcf.samples, path=self.gs_uri)
+            self._test_cmd(terra_notebook_utils.cli.commands.vcf.samples, path=self.gs_uri)
 
         with self.subTest("Test local object"):
-            self._test_cmd(terra_notebook_utils.cli.vcf.samples, path="tests/fixtures/non_block_gzipped.vcf.gz")
+            self._test_cmd(terra_notebook_utils.cli.commands.vcf.samples,
+                           path="tests/fixtures/non_block_gzipped.vcf.gz")
 
     @testmode("controlled_access")
     def test_samples_drs(self):
         with self.subTest("Test drs:// object"):
-            self._test_cmd(terra_notebook_utils.cli.vcf.samples,
+            self._test_cmd(terra_notebook_utils.cli.commands.vcf.samples,
                            path=self.vcf_drs_url)
 
     def test_stats(self):
         with self.subTest("Test gs:// object"):
-            self._test_cmd(terra_notebook_utils.cli.vcf.stats, path=self.gs_uri)
+            self._test_cmd(terra_notebook_utils.cli.commands.vcf.stats, path=self.gs_uri)
 
         with self.subTest("Test local object"):
-            self._test_cmd(terra_notebook_utils.cli.vcf.stats, path="tests/fixtures/non_block_gzipped.vcf.gz")
+            self._test_cmd(terra_notebook_utils.cli.commands.vcf.stats, path="tests/fixtures/non_block_gzipped.vcf.gz")
 
     @testmode("controlled_access")
     def test_stats_drs(self):
         with self.subTest("Test drs:// object"):
-            self._test_cmd(terra_notebook_utils.cli.vcf.stats,
+            self._test_cmd(terra_notebook_utils.cli.commands.vcf.stats,
                            path=self.vcf_drs_url)
 
 
 @testmode("workspace_access")
-class TestTerraNotebookUtilsCLI_Workspace(CLITestMixin, unittest.TestCase):
+class TestCLI_Workspace(CLITestMixin, unittest.TestCase):
     def test_list(self):
-        self._test_cmd(terra_notebook_utils.cli.workspace.list_workspaces)
+        self._test_cmd(terra_notebook_utils.cli.commands.workspace.list_workspaces)
 
     def test_get(self):
-        self._test_cmd(terra_notebook_utils.cli.workspace.get_workspace,
+        self._test_cmd(terra_notebook_utils.cli.commands.workspace.get_workspace,
                        workspace=WORKSPACE_NAME,
                        workspace_namespace="firecloud-cgl")
 
 
 @testmode("workspace_access")
-class TestTerraNotebookUtilsCLI_Profile(CLITestMixin, unittest.TestCase):
+class TestCLI_Profile(CLITestMixin, unittest.TestCase):
     def list_workspace_namespaces(self):
-        self._test_cmd(terra_notebook_utils.cli.profile.list_workspace_namespaces)
+        self._test_cmd(terra_notebook_utils.cli.commands.profile.list_workspace_namespaces)
 
 
 # These tests will only run on `make dev_env_access_test` command as they are testing DRS against Terra Dev env
 @testmode("dev_env_access")
-class TestTerraNotebookUtilsCLI_DRSInDev(CLITestMixin, unittest.TestCase):
+class TestCLI_DRSInDev(CLITestMixin, unittest.TestCase):
     jade_dev_url = "drs://jade.datarepo-dev.broadinstitute.org/v1_0c86170e-312d-4b39-a0a4-" \
                    "2a2bfaa24c7a_c0e40912-8b14-43f6-9a2f-b278144d0060"
     expected_crc32c = "/VKJIw=="
@@ -176,7 +175,7 @@ class TestTerraNotebookUtilsCLI_DRSInDev(CLITestMixin, unittest.TestCase):
     def test_copy(self):
         with self.subTest("test copy to local path"):
             with NamedTemporaryFile() as tf:
-                self._test_cmd(terra_notebook_utils.cli.drs.drs_copy,
+                self._test_cmd(terra_notebook_utils.cli.commands.drs.drs_copy,
                                drs_url=self.jade_dev_url,
                                dst=tf.name,
                                workspace=WORKSPACE_NAME,
@@ -187,7 +186,7 @@ class TestTerraNotebookUtilsCLI_DRSInDev(CLITestMixin, unittest.TestCase):
 
         with self.subTest("test copy to gs bucket"):
             key = "test-drs-cli-object"
-            self._test_cmd(terra_notebook_utils.cli.drs.drs_copy,
+            self._test_cmd(terra_notebook_utils.cli.commands.drs.drs_copy,
                            drs_url=self.jade_dev_url,
                            dst=f"gs://{WORKSPACE_BUCKET}/{key}",
                            workspace=WORKSPACE_NAME,
@@ -201,14 +200,14 @@ class TestTerraNotebookUtilsCLI_DRSInDev(CLITestMixin, unittest.TestCase):
 
 
 @testmode("controlled_access")
-class TestTerraNotebookUtilsCLI_DRS(CLITestMixin, unittest.TestCase):
+class TestCLI_DRS(CLITestMixin, unittest.TestCase):
     drs_url = "drs://dg.4503/95cc4ae1-dee7-4266-8b97-77cf46d83d35"
     expected_crc32c = "LE1Syw=="
 
     def test_copy(self):
         with self.subTest("test local"):
             with NamedTemporaryFile() as tf:
-                self._test_cmd(terra_notebook_utils.cli.drs.drs_copy,
+                self._test_cmd(terra_notebook_utils.cli.commands.drs.drs_copy,
                                drs_url=self.drs_url,
                                dst=tf.name,
                                workspace=WORKSPACE_NAME,
@@ -219,7 +218,7 @@ class TestTerraNotebookUtilsCLI_DRS(CLITestMixin, unittest.TestCase):
 
         with self.subTest("test gs"):
             key = "test-drs-cli-object"
-            self._test_cmd(terra_notebook_utils.cli.drs.drs_copy,
+            self._test_cmd(terra_notebook_utils.cli.commands.drs.drs_copy,
                            drs_url=self.drs_url,
                            dst=f"gs://{WORKSPACE_BUCKET}/{key}",
                            workspace=WORKSPACE_NAME,
@@ -267,7 +266,7 @@ class TestTerraNotebookUtilsCLI_DRS(CLITestMixin, unittest.TestCase):
                 self._run_cmd(cmd)
 
 @testmode("workspace_access")
-class TestTerraNotebookUtilsCLI_Table(CLITestMixin, unittest.TestCase):
+class TestCLI_Table(CLITestMixin, unittest.TestCase):
     common_kwargs = dict(workspace=WORKSPACE_NAME, workspace_namespace=WORKSPACE_GOOGLE_PROJECT)
 
     @classmethod
@@ -294,13 +293,13 @@ class TestTerraNotebookUtilsCLI_Table(CLITestMixin, unittest.TestCase):
         self.cell_value = self.table_data[self.row_index][self.column]
 
     def test_list(self):
-        self._test_cmd(terra_notebook_utils.cli.table.list_tables)
+        self._test_cmd(terra_notebook_utils.cli.commands.table.list_tables)
 
     def test_list_rows(self):
-        self._test_cmd(terra_notebook_utils.cli.table.list_rows, table="simple_germline_variation")
+        self._test_cmd(terra_notebook_utils.cli.commands.table.list_rows, table="simple_germline_variation")
 
     def test_get_row(self):
-        out = self._test_cmd(terra_notebook_utils.cli.table.get_row,
+        out = self._test_cmd(terra_notebook_utils.cli.commands.table.get_row,
                              table=self.table,
                              row=self.entity_id)
         row_name, data = out.split(maxsplit=1)
