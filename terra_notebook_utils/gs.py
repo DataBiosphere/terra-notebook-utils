9afc2cb9b96453f7c9906d336abc6a3e1e96c793
Lon Blauvelt <lblauvel@ucsc.edu>
2022-03-10 13:57:02 -0800

    Tweaks.

--- terra_notebook_utils/gs.py
+++ terra_notebook_utils/gs.py
@@ -143,16 +143,12 @@ def get_signed_url(bucket: str,
     signature = binascii.hexlify(creds.signer.sign(string_to_sign)).decode()
     return f'https://{host}{canonical_uri}?{canonical_query_string}&x-goog-signature={signature}'
 
-def list_bucket(prefix="", bucket=os.environ['WORKSPACE_BUCKET']):
-    """
-    Lists blobs in "gs://bucket/prefix/"; used by BYOD notebook. Note that Google's list_blobs()
-    is partially depreciated, and later we may need to call it like this instead:
-    gs.get_client().list_blobs(stripped_bucket, prefix=prefix)
-    """
-    stripped_bucket = strip_gs(bucket)
-    for blob in get_client().bucket(stripped_bucket).list_blobs(prefix=prefix):
-        yield blob.name
+def list_bucket(prefix: str = '', bucket: Optional[str] = None):
+    """Lists blobs in "gs://bucket/prefix/"."""
+    bucket = bucket or os.environ['WORKSPACE_BUCKET']
+    if bucket.startswith('gs://'):
+        bucket = bucket[len('gs://'):]
 
-def strip_gs(bucket_with_gs):
-    # Turns "gs://some-bucket" into "some-bucket" which is the format list_blobs() requires
-    return bucket_with_gs[5:]
+    # TODO: upgrade Google lib to use new 'gs.get_client().list_blobs(bucket, prefix=prefix)'
+    for blob in get_client().bucket(bucket).list_blobs(prefix=prefix):
+        yield blob.name
