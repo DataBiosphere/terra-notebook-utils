a99fc7768f10f1379b7a020ff564c23ba17d3cc3
Michael Baumann <michael@pacificcoastsolutions.net>
2023-01-23 18:29:51 -0800

    Added unit test for utils.get_execution_context

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,7 @@ import sys
 import time
 import random
 import unittest
+from unittest import mock
 from uuid import uuid4
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # noqa
@@ -41,6 +42,37 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(123, utils.js_get("path.does.not.exist", data, default=123))
         with self.assertRaises(KeyError):
             utils.js_get("foo.not.in.dict", data)
+
+    def test_get_execution_context(self):
+        """
+        Note: The implementation/logic of this function is likely to change.
+        This test currently checks the interim behavior.
+        See comments in utils.get_execution_context for more information.
+
+        See this article for info about mock patch for environment variables:
+        https://adamj.eu/tech/2020/10/13/how-to-mock-environment-variables-with-pythons-unittest/
+        See this article for info about clearing the @lru_cache when unit testing:
+        https://rishabh-ink.medium.com/testing-lru-cache-functions-in-python-with-pytest-33dd5757d11c
+        """
+        from terra_notebook_utils.utils import get_execution_context, ExecutionContext,\
+            ExecutionEnvironment, ExecutionPlatform
+
+        with self.subTest("Typical: Terra, Google"):
+            get_execution_context.cache_clear()
+            env_vars = {"WORKSPACE_BUCKET": "gs://dummy_bucket/dummy/key/path"}
+            with mock.patch.dict(os.environ, env_vars, clear=True):
+                exc = get_execution_context()
+                self.assertEqual(ExecutionEnvironment.TERRA_WORKSPACE, exc.execution_environment)
+                self.assertEqual(ExecutionPlatform.GOOGLE, exc.execution_platform)
+
+        with self.subTest("Current default guess: Terra, Azure"):
+            get_execution_context.cache_clear()
+            env_vars = {}  # No WORKSPACE_BUCKET in current Azure context
+            with mock.patch.dict(os.environ, env_vars, clear=True):
+                exc = get_execution_context()
+                self.assertEqual(ExecutionEnvironment.TERRA_WORKSPACE, exc.execution_environment)
+                self.assertEqual(ExecutionPlatform.AZURE, exc.execution_platform)
+
 
 if __name__ == '__main__':
     unittest.main()
